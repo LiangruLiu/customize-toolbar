66475ec6522efa292b6625e65c3da0d37ca42c9b
123 <456>
2022-07-08 00:02:44 +0800

    0

--- sources/extension.js
+++ sources/extension.js
@@ -1,17 +1,18 @@
-// 20220706
+// 20220707
 "use strict"
 const vscode = require ("vscode")  // 请忽略提示，千万不要点击自动修复
 const feature = require ("./feature.js")
 
 
 exports.activate = function (context) {
+	const getCfg = vscode.workspace.getConfiguration ("CustomizeToolbar").get
 	context.subscriptions.push( vscode.workspace.onDidChangeConfiguration( (event) => {
-		const getCfg = vscode.workspace.getConfiguration ("CustomizeToolbar").get
 		if (event.affectsConfiguration ("CustomizeToolbar.buttonConfig")) {
 			feature.updateButtonConfig (context, getCfg ("buttonConfig"))
 			feature.promptToReload ()
 		}
 	}))
+	feature.registerCommands (context, getCfg ("buttonConfig"))
 }
 
 
--- sources/feature.js
+++ sources/feature.js
@@ -7,31 +7,38 @@ const path = require ("path")
 
 // 本函数通过修改package.json来实现功能
 exports.updateButtonConfig = function (context, btnCfg) {
+	const builtinFolder = "./images/builtinIcons"
+	const userFolder = "./images/userIcons"
+	// 清空用户图标文件夹
+	for (let file of fs.readdirSync (userFolder))
+		fs.unlinkSync (`${userFolder}/${file}`)
 	let commands = []
 	let keybindings = []
 	let buttons = []
 	for (let idx = 0; idx < btnCfg.length; idx ++) {
+		const icon = btnCfg[idx]["icon"]
+		if (typeof icon === "object")
+			for (let key in icon)
+				if (icon[key].startsWith ("builtin/")) {
+					icon[key] = icon[key].replace ("builtin", builtinFolder)
+				} else {
+					newPath = `${userFolder}/btn${idx+1}_${key}.svg`
+					fs.copyFileSync (icon[key], newPath)
+					icon[key] = newPath
+				}
 		const cmdName = `customize-toolbar.command-${idx+1}`
-		context.subscriptions.push( vscode.commands.registerCommand( cmdName, () => {
-			vscode.commands.executeCommand (btnCfg[idx]["command"])  // .then(function () {})
-		}))
-		if (typeof btnCfg[idx]["icon"] === "object")
-			for (key in btnCfg[idx]["icon"])
-				if (btnCfg[idx]["icon"][key].startsWith ("builtin/"))
-					btnCfg[idx]["icon"][key] = btnCfg[idx]["icon"][key]
-						.replace ("builtin", "./images/builtinIcons")
 		commands[idx] = {
 			"command": cmdName,
 			"category": "Customize Toolbar",
 			"title": btnCfg[idx]["name"],
-			"icon": btnCfg[idx]["icon"]
+			"icon": icon
 		}
-		if (idx+1 <= 8)
-			keybindings[idx] = {
-				"command": cmdName,
-				"key": `ctrl+alt+${idx+1}`,
-				"mac": `shift+cmd+${idx+1}`
-			}
+		// if (idx+1 <= 8)
+		// 	keybindings[idx] = {
+		// 		"command": cmdName,
+		// 		"key": `ctrl+alt+${idx+1}`,
+		// 		"mac": `shift+cmd+${idx+1}`
+		// 	}
 		buttons[idx] = {
 			"group": `navigation@${idx+1}`,
 			// "when": `config.CustomizeToolbar.buttonConfig.length >= ${idx+1} && resourceFilename =~ ${btnCfg[idx]["when"]}`,
@@ -54,7 +61,20 @@ exports.promptToReload = function () {
 	const action = ["Reload", "Not Now"]
 	vscode.window.showInformationMessage (message, ...action) .then( (selectedAction) => {
 		if (selectedAction === "Reload") {
-			vscode.commands.executeCommand ("workbench.action.reloadWindow");
+			vscode.commands.executeCommand ("workbench.action.reloadWindow")
 		}
 	})
+}
+
+
+exports.registerCommands = function (context, btnCfg) {
+	for (let idx = 0; idx < btnCfg.length; idx ++) {
+		const cmdName = `customize-toolbar.command-${idx+1}`
+		context.subscriptions.push( vscode.commands.registerCommand( cmdName, () => {
+			vscode.commands.executeCommand (btnCfg[idx]["command"])
+		}))
+		// 因为package.json不能写注释，所以就记在这里了
+		// 注意buttonConfig中的command不能填名字而要填标识符，具体可以去快捷键列表里找
+		// 比如收放终端，要写workbench.action.terminal.toggleTerminal而非view.toggleTerminal
+	}
 }
